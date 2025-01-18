5d70dcc7fbf527b74b96a3841c32cdd12ac8d4e1
Aaron <aarondeo30@gmail.com>
2025-01-17 21:25:18 -0800

    added ball speed

--- Assets/Scenes/BallController.cs
+++ Assets/Scenes/BallController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    [SerializeField] private Rigidbody sphereRigidbody;
+    [SerializeField] private float ballSpeed = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,21 +13,26 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 inputVector = Vector2.zero;
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("User's Input: W");
+            inputVector += Vector2.up;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("User's Input: A");
+            inputVector += Vector2.left;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            Debug.Log("User's Input: S");
+            inputVector += Vector2.down;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("User's Input: D");
+            inputVector += Vector2.right;
         }
+        Vector3 inputXZPlane = new Vector3(inputVector.x,0,inputVector.y);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+        Debug.Log("Resultant Vector: " + inputVector);
+        Debug.Log("Resultant 3D Vector: " + inputXZPlane);
     }
 }
